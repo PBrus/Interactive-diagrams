1f2e87739d4dd8db5176706c3302eda0bdefdcb3
Przemysław Bruś <b_przemas@interia.pl>
2017-01-08 14:10:12 +0100

    Update help description
    Add more information about a header of an input file.

--- photometric_diagrams.py
+++ photometric_diagrams.py
@@ -5,12 +5,13 @@ from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter as tefo	# allow to use a new line within argparse strings
 
 ## parse script arguments
-argparser = ArgumentParser(prog='photometric_diagrams.py', description='>> Interactive CCD and CMD diagrams <<\n\n \
-Requires Python 2.7 with:\n  * numpy\n  * argparse\n  * matplotlib\n\n', 
+argparser = ArgumentParser(prog='photometric_diagrams.py', description='>> Interactive \
+CCD and CMD diagrams <<\n\n Requires Python 2.7 with:\n  * numpy\n  * argparse\n  * matplotlib\n\n', 
 epilog='Copyright (c) 2017 Przemysław Bruś', formatter_class=tefo)
-argparser.add_argument('input_file', help='must contain columns with data and a header')
+argparser.add_argument('input_file', help='must contain columns with data and a one-line header\n\
+The header must be preceded by # sign. Labels from the header \nwill be used to sign axes on charts')
 argparser.add_argument('--col', help="columns which should be used to create \
-the diagram\nNegative value reverses range's axis. There is no limit to the diagrams", \
+the diagram\nA negative value reverses range's axis. There is no limit to the diagrams", \
 nargs=2, dest='col', action='append', metavar=('col_x', 'col_y'), required=True)
 argparser.add_argument('--grp', help='group of stars which can be marked by color\n\
 /file/ should contain only one column with ID numbers of stars\nIf this option is used, \
@@ -21,7 +22,7 @@ matplotlib documentation. Gray and red colors are reserved\nfor plotting the bac
 selection of points, respectively', nargs=2, dest='grp', action='append', metavar=('file', 'color'))
 argparser.add_argument('-t', help='talkative mode. Printing feedback with every click', \
 action='store_true')
-argparser.add_argument('-v', '--version', action='version', version='%(prog)s\n * Version: 2017-01-06\n \
+argparser.add_argument('-v', '--version', action='version', version='%(prog)s\n * Version: 2017-01-08\n \
 * Licensed under the MIT license:\n   http://opensource.org/licenses/MIT\n * Copyright (c) 2017 Przemysław Bruś')
 args = argparser.parse_args()
 
