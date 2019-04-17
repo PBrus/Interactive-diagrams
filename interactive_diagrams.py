ea244ff8754fe5ce5806504763f325b9c64d299d
Przemysław Bruś <brus@astro.uni.wroc.pl>
2019-04-17 23:41:34 +0200

    Update the main script

--- interactive_diagrams.py
+++ interactive_diagrams.py
@@ -1,29 +1,82 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser
-from argparse import RawTextHelpFormatter as tefo
+from importlib import import_module
+from argparse import ArgumentParser, RawTextHelpFormatter
+from textwrap import dedent
 from idgrms.data import *
 from idgrms.plotdgrms import *
 
 
-argparser = ArgumentParser(prog='photometric_diagrams.py', description='>> Interactive \
-CCD and CMD diagrams <<\n\n Requires Python 2.7 with:\n  * numpy\n  * argparse\n  * matplotlib\n\n',
-epilog='Copyright (c) 2017 Przemysław Bruś', formatter_class=tefo)
-argparser.add_argument('input_file', help='must contain columns with data and a one-line header\n\
-The header must be preceded by # sign. Labels from the header \nwill be used to sign axes on charts')
-argparser.add_argument('--col', help="columns which should be used to create \
-the diagram\nA negative value reverses range's axis. There is no limit to the diagrams", \
-nargs=2, dest='col', action='append', metavar=('col_x', 'col_y'), required=True, type=int)
-argparser.add_argument('--grp', help='group of stars which can be marked by color\n\
-/file/ should contain only one column with ID numbers of stars\nIf this option is used, \
-also /input/ must have ID numbers of stars\nin the first column. The variable /color/ \
-stores color which marks\nthe points from the /file/. The color can be specified \
-by an html\nhex string ("#4f21b7") or literally (blue or b). For more details\nsee the \
-matplotlib documentation. Gray and red colors are reserved\nfor plotting the background and \
-selection of points, respectively', nargs=2, dest='grp', action='append', metavar=('file', 'color'))
-argparser.add_argument('-t', help='talkative mode. Printing feedback with every click', \
-action='store_true')
-argparser.add_argument('-v', '--version', action='version', version='%(prog)s\n * Version: 2017-02-23\n \
-* Licensed under the MIT license:\n   http://opensource.org/licenses/MIT\n * Copyright (c) 2017 Przemysław Bruś')
+argparser = ArgumentParser(
+    prog='interactive_diagrams.py',
+    description='>> Display and interact with diagrams <<',
+    epilog='Copyright (c) 2019 Przemysław Bruś',
+    formatter_class=RawTextHelpFormatter
+)
+argparser.add_argument(
+    'data_file',
+    help=dedent('''\
+    The name of a file which must contain columns with data:
+    id value_1 value_2 value_3 ...
+    ------------------------------
+    int float float float ...
+
+    Only the first column must be an integer, the rest must
+    be float. Additionally, the file must contain a one-line
+    header. The header must be preceded by # sign. Labels from
+    the header are used to mark axes on diagrams.
+    ''')
+)
+argparser.add_argument(
+    '--col',
+    help=dedent('''\
+    The columns numbers which will be used to create diagrams.
+
+    A negative number reverses an axis range. There is no limit
+    of a diagrams amount, just add another --col arguments.
+    '''),
+    nargs=2,
+    dest='columns',
+    action='append',
+    metavar=('x', 'y'),
+    required=True,
+    type=int
+)
+argparser.add_argument(
+    '--grp',
+    help=dedent('''\
+    Group of stars which can be marked by [color].
+
+    [file] should contain only one column with ID numbers. If this
+    option is used, [data_file] must have the same ID numbers in
+    the first column.
+
+    The [color] marks points from [file]. The color can be specified
+    by an html hex string ("#4f21b7") or literally (blue or b).
+    '''),
+    nargs=2,
+    dest='grp',
+    action='append',
+    metavar=('file', 'color')
+)
+argparser.add_argument(
+    '-t',
+    help=dedent('''\
+    Talkative mode. Print feedback with every click.
+    '''),
+    action='store_true'
+)
+argparser.add_argument(
+    '-v',
+    '--version',
+    action='version',
+    version=dedent('''\
+    %(prog)s
+    * Version: ''' + import_module('idgrms').__version__ + '''
+    * Licensed under the MIT license:
+    * http://opensource.org/licenses/MIT
+    * ''' + argparser.epilog)
+)
+
 args = argparser.parse_args()
-trigger_windows(args.input_file, args.col, args.grp, args.t)
+trigger_windows(args.data_file, args.columns, args.grp, args.t)
